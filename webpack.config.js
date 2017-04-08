16e8526cba875a27993bf7ec95092c68801c811c
Alexander Thurn <at@froso.de>
2017-04-08 14:28:13 +0200

    uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,54 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     entry: "./src/js/main.js",
     output: {
-        path: __dirname,
+        path: __dirname + '/dist/',
         filename: "bundle.js"
     },
     module: {
         loaders: [
             {test: /\.css$/, loader: "style!css"}
         ]
-    }
+    },
+
+    plugins: [
+        new UglifyJsPlugin({
+            compress: {
+                sequences: true,
+                properties: true,
+                drop_debugger: true,
+                dead_code: true,
+                unsafe: true,
+                conditionals: true,
+                comparisons: true,
+                evaluate: true,
+                booleans: true,
+                unused: true,
+                loops: true,
+                cascade: true,
+                keep_fargs: false,
+                if_return: true,
+                join_vars: true,
+                drop_console: true
+            },
+            'mangle-props': true,
+            mangle: true,
+            beautify: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: "src/index.htm",
+            minify: {
+                minifyJS: true,
+                removeEmptyAttributes: true
+            },
+            inlineSource: '.(js|css)$',
+            cache: true
+        }),
+        new HtmlWebpackInlineSourcePlugin()
+    ]
 };
+
